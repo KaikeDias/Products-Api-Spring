c6842d83999123da784ca955e607734dac1287fd
Kaike <kaikedias98@gmail.com>
2023-09-15 01:30:44 -0300

    initial commit

---
+++ src/main/java/com/example/productapirest/ProductApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.productapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApiRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/productapirest/ProductApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
