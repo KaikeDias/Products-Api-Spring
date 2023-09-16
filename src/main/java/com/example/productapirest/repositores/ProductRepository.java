607a781578610928e91b80e34b54ff51e746e48c
Kaike <kaikedias98@gmail.com>
2023-09-16 15:19:49 -0300

    feat: add product repository

---
+++ src/main/java/com/example/productapirest/repositores/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.productapirest.repositores;
+
+import com.example.productapirest.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+}
